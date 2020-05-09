b23a1beab1a72dffbf82489174cf352f0dfccb87
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 14:50:58 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
-    t.integer "network_id"
   end
 
 end
