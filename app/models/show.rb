bdc7ead40041e163b5a03cba38c89d9a96f730bf
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 15:28:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,7 +3,9 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
 
   def actors_list
-    binding.pry
+    self.actors.collect do |actor|
+      "#{actor.first_name} #{actor.last_name}"
+    end
   end
 
 end
