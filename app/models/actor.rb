6b9b9efee6100b77c5d942ebdef7cdb07cc913d0
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 15:00:11 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,7 +2,7 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
 
-  # def full_name
-  #   "#{actors.first_name} #{actors.last_name}"
-  # end
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
