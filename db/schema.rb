ea2dd1f28724307e053363ab69049786e662a605
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 14:50:47 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6) do
+ActiveRecord::Schema.define(version: 5) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 6) do
     t.string "name"
     t.integer "actor_id"
     t.integer "show_id"
-    t.string "catchphrase"
   end
 
   create_table "networks", force: :cascade do |t|
