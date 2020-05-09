882ccf1b0f28775f28f56ba92d2b261141c9290e
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 14:53:51 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -3,6 +3,7 @@ class CreateActors < ActiveRecord::Migration[5.2]
     create_table :actors do |t|
       t.string :first_name
       t.string :last_name
+      t.integer :character_id
     end
   end
 end
