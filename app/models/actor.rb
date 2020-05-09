17e8c0f4d18057d97af42b2c7ea0e92ae4a3e51c
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-09 15:14:49 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    "#{self.characters} - #{self.shows}"
+    self.characters.collect do |character|
+      "#{self.character} - #{self.show}"
   end
 end
